3f22b5777534f68a3812cc231bd33ee979866878
Moosieus <cameron.duley99@gmail.com>
2023-05-31 01:03:37 -0400

    register with hex

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule A2S.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "Elixir_A2S",
+      source_url: "https://github.com/Moosieus/elixir-a2s"
     ]
   end
 
@@ -21,8 +25,19 @@ defmodule A2S.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description do
+    "An implementation of Valve's A2S protocol for Elixir."
+  end
+
+  defp package do
+    [
+      name: "elixir_a2s",
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/Moosieus/elixir-a2s"}
     ]
   end
 end
