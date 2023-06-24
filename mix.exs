6d5103b8163f796ed074818d8d3b5c8690e2a4b9
Moosieus <cameron.duley99@gmail.com>
2023-06-24 17:38:10 -0400

    fix versioning

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule A2S.MixProject do
   def project do
     [
       app: :elixir_a2s,
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       description: "An implementation of Valve's A2S protocol for Elixir.",
