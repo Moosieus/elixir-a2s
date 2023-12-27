eced5368628fb4f28d69a3eebd62c4a7720bde73
Moosieus <cameron.duley99@gmail.com>
2023-12-27 00:07:14 -0500

    Update statem: - Use struct for data store - Use postpone instead of queue - Remove redundant callbacks - Format file

--- lib/a2s/statem.ex
+++ lib/a2s/statem.ex
@@ -1,11 +1,10 @@
 defmodule A2S.Statem do
   @moduledoc """
-  A state machine process responsible for handling all A2S queries to a game server running at the given address. Queries must be performed sequentially per address, as A2S provides no way to associate what replies associate to what responses.
+  A state machine process responsible for handling all A2S queries to a game server running at the given address.
+  Queries must be performed sequentially per address as A2S provides no way to associate what replies associate to what responses.
 
   Each instance should exit normally after a certain interval of inactivity (currently hard-coded to 2 minutes).
   """
-
-  @type init_args() :: {:inet.ip_address(), :inet.port_number()}
 
   @behaviour :gen_statem
 
@@ -14,20 +13,17 @@ defmodule A2S.Statem do
   @impl :gen_statem
   def callback_mode, do: :handle_event_function
 
-  ## API
-
-  @spec start_link(init_args) :: :ignore | {:error, any} | {:ok, pid}
-  def start_link(address) do
-    :gen_statem.start_link(via_registry(address), __MODULE__, address, [])
+  defmodule Data do
+    defstruct [:address, :caller, :query, :total, :parts]
   end
 
-  @spec stop({:inet.ip_address(), :inet.port_number()}, any) :: :ok
-  def stop(address, reason) do
-    address |> via_registry |> GenServer.stop(reason)
-  end
+  ## Initialization
+
+  @type init_args() :: {:inet.ip_address(), :inet.port_number()}
 
   @spec child_spec(init_args) :: Supervisor.child_spec()
   def child_spec(address) do
+    # need to revisit this child specification
     %{
       id: __MODULE__,
       start: {__MODULE__, :start_link, [address]},
@@ -35,115 +31,144 @@ defmodule A2S.Statem do
     }
   end
 
-  ## Callbacks
+  @spec start_link(init_args) :: :ignore | {:error, any} | {:ok, pid}
+  def start_link(address) do
+    :gen_statem.start_link(via_registry(address), __MODULE__, address, [])
+  end
 
   @impl :gen_statem
   def init(address) do
-    {:ok, :idle, %{address: address, queue: []}, state_timeout()}
+    {:ok, :idle, %Data{address: address}, idle_timeout()}
   end
 
-  @impl :gen_statem
-  def handle_event(:internal, :process_next, :idle, %{queue: []} = data) do
-    {:keep_state, data, state_timeout()}
+  ## Stopping
+
+  @spec stop({:inet.ip_address(), :inet.port_number()}, any) :: :ok
+  def stop(address, reason) do
+    via_registry(address)
+    # should this be `:gen_statem.stop(reason)`?
+    |> GenServer.stop(reason)
   end
 
+  defp via_registry(address), do: {:via, Registry, {:a2s_registry, address}}
+
+  ## State Machine Callbacks
+
+  # Received a query to perform
+
   @impl :gen_statem
-  def handle_event(:internal, :process_next, :idle, %{address: address, queue: [{caller, query} | queue]} = data) do
+  def handle_event({:call, from}, query, :idle, data) do
+    %Data{address: address} = data
+
     :ok = GenServer.call(A2S.UDP, {address, A2S.challenge_request(query)})
 
-    data = data
-    |> Map.put(:caller, caller)
-    |> Map.put(:query, query)
-
-    {:next_state, :await_challenge, %{data | queue: queue}, recv_timeout()}
+    {
+      :next_state,
+      :await_challenge,
+      %Data{data | caller: from, query: query},
+      recv_timeout()
+    }
   end
 
+  # A2S queries don't have IDs so they must be processed sequentially to avoid ambiguity.
   @impl :gen_statem
-  def handle_event(:cast, packet, :await_challenge, %{address: address, caller: caller, query: query} = data) do
+  def handle_event({:call, _from}, _query_type, _state, _data) do
+    {:keep_state_and_data, :postpone}
+  end
+
+  # Received a packet
+
+  @impl :gen_statem
+  def handle_event(:cast, packet, :await_challenge, data) do
+    %Data{
+      address: address,
+      query: query
+    } = data
+
     case A2S.parse_challenge(packet) do
+      {:immediate, msg} ->
+        reply_and_next(msg, data)
+
       {:challenge, challenge} ->
         :ok = GenServer.call(A2S.UDP, {address, A2S.sign_challenge(query, challenge)})
         {:next_state, :await_response, data, recv_timeout()}
 
-      {:immediate, msg} ->
-        reply_and_next(msg, caller, data)
-
       {:multipacket, {header, _body} = part} ->
-        data = data
-        |> Map.put(:total, header.total)
-        |> Map.put(:parts, [part])
-
-        {:next_state, :collect_multipacket, data, recv_timeout()}
+        {
+          :next_state,
+          :collect_multipacket,
+          %Data{data | total: header.total, parts: [part]},
+          recv_timeout()
+        }
     end
   end
 
   @impl :gen_statem
-  def handle_event(:cast, packet, :await_response, %{caller: caller} = data) do
+  def handle_event(:cast, packet, :await_response, data) do
     case A2S.parse_response(packet) do
       {:multipacket, {header, _body} = part} ->
-        data = data
-        |> Map.put(:total, header.total)
-        |> Map.put(:parts, [part])
+        {
+          :next_state,
+          :collect_multipacket,
+          %Data{data | total: header.total, parts: [part]},
+          recv_timeout()
+        }
 
-        {:next_state, :collect_multipacket, data, recv_timeout()}
-
+      # reply with whatever the result is.
       msg ->
-        reply_and_next(msg, caller, data)
+        reply_and_next(msg, data)
     end
   end
 
   @impl :gen_statem
-  def handle_event(:cast, packet, :collect_multipacket, %{caller: caller, total: total, parts: parts} = data) do
+  def handle_event(:cast, packet, :collect_multipacket, data) do
+    %Data{
+      total: total,
+      parts: parts
+    } = data
+
     {:multipacket, part} = A2S.parse_response(packet)
     parts = [part | parts]
 
     if Enum.count(parts) === total do
-      data = clear_multipacket(data)
-
-      parts
-      |> A2S.parse_multipacket_response()
-      |> reply_and_next(caller, data)
+      A2S.parse_multipacket_response(parts)
+      |> reply_and_next(data)
     else
-      {:next_state, :collect_multipacket, %{data | parts: parts}, recv_timeout()}
+      {
+        :next_state,
+        :collect_multipacket,
+        %Data{data | parts: parts},
+        recv_timeout()
+      }
     end
   end
 
-  # Add requests to queue
+  # Received a timeout
 
   @impl :gen_statem
-  def handle_event({:call, caller}, query_type, :idle, %{queue: []} = data) do
-    {:keep_state, %{data | queue: [{caller, query_type}]}, [{:next_event, :internal, :process_next}, recv_timeout()]}
-  end
-
-  @impl :gen_statem
-  def handle_event({:call, caller}, query_type, _state, %{queue: queue} = data) do
-    {:keep_state, %{data | queue: queue ++ {caller, query_type}}, recv_timeout()}
-  end
-
-  ## Timeouts
-
   def handle_event(:state_timeout, :idle_timeout, :idle, _data) do
     {:stop, :normal}
   end
 
-  def handle_event(:state_timeout, :recv_timeout, _state, %{caller: caller} = data) do
-    data = clear_multipacket(data)
-    reply_and_next({:error, :recv_timeout}, caller, data)
+  @impl :gen_statem
+  def handle_event(:state_timeout, :recv_timeout, _state, data) do
+    reply_and_next({:error, :recv_timeout}, data)
   end
 
-  ## Functions
-
-  defp reply_and_next(msg, caller, data) do
+  defp reply_and_next(msg, %Data{address: address, caller: caller}) do
     :gen_statem.reply(caller, msg)
-    data = clear_query(data)
-    {:next_state, :idle, data, [{:next_event, :internal, :process_next}, recv_timeout()]}
+
+    {
+      :next_state,
+      :idle,
+      %Data{address: address},
+      recv_timeout()
+    }
   end
 
-  defp clear_query(data), do: data |> Map.delete(:caller) |> Map.delete(:query)
+  ## Timeout Definitions
 
-  defp clear_multipacket(data), do: data |> Map.delete(:total) |> Map.delete(:parts)
-
-  defp state_timeout() do
+  defp idle_timeout() do
     timeout = :persistent_term.get({__MODULE__, :idle_timeout})
     {:state_timeout, timeout, :idle_timeout}
   end
@@ -152,6 +177,4 @@ defmodule A2S.Statem do
     timeout = :persistent_term.get({__MODULE__, :recv_timeout})
     {:state_timeout, timeout, :recv_timeout}
   end
-
-  defp via_registry(address), do: {:via, Registry, {:a2s_registry, address}}
 end
